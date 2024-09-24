e30a6f99124a4002aafa087c6b6fb8bf7e8009a4
unknown <milanmartis@gmail.com>
2024-09-24 21:10:15 +0200

    vxx33

--- website/static/js/service-worker.js
+++ website/static/js/service-worker.js
@@ -1,21 +1,13 @@
 self.addEventListener('push', event => {
     const data = event.data.json();
-    console.log('Push notifikácia prijatá:', data);
+    console.log('Push notifikácia prijatá', data);
 
     const options = {
-        title: "kuk",
         body: data.body,
-        icon: '/static/img/icon.png'  // Cesta k ikonke notifikácie
+        icon: data.icon
     };
 
     event.waitUntil(
         self.registration.showNotification(data.title, options)
-    );
-});
-
-self.addEventListener('notificationclick', event => {
-    event.notification.close();  // Zatvorí notifikáciu
-    event.waitUntil(
-        clients.openWindow('/')  // Otvorí aplikáciu, keď používateľ klikne na notifikáciu
     );
 });
