099dbb7d5b0ecd8e45868f0baf5ec293544864fe
unknown <milanmartis@gmail.com>
2024-09-24 12:41:08 +0200

    vxx20

--- website/static/js/service-worker.js
+++ website/static/js/service-worker.js
@@ -1,13 +1,30 @@
 self.addEventListener('push', event => {
-    const data = event.data.json();
-    console.log('Push notifikácia prijatá', data);
+    const data = event.data ? event.data.json() : {};
 
+    // Nastavenie predvolených hodnôt pre notifikáciu, ak niektoré údaje chýbajú
     const options = {
-        body: data.body,
-        icon: data.icon
+        body: data.body || 'Nová správa!',
+        icon: data.icon || '/static/default-icon.png',
+        tag: data.tag || 'default-tag', // Tag pre zlúčenie notifikácií
+        actions: [
+            { action: 'view', title: 'Zobraziť' },
+            { action: 'dismiss', title: 'Zatvoriť' }
+        ],
+        data: {
+            url: data.url || '/' // URL na otvorenie po kliknutí na notifikáciu
+        }
     };
 
     event.waitUntil(
-        self.registration.showNotification(data.title, options)
+        self.registration.showNotification(data.title || 'Nová notifikácia', options)
+    );
+});
+
+// Spracovanie kliknutia na notifikáciu
+self.addEventListener('notificationclick', event => {
+    event.notification.close(); // Zatvorenie notifikácie po kliknutí
+
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
     );
 });
