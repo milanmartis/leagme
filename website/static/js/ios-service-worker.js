72a7aa6730f91bf497083e7091641d42c15d113e
unknown <milanmartis@gmail.com>
2024-09-29 20:57:56 +0200

    vxxx119

--- website/static/js/ios-service-worker.js
+++ website/static/js/ios-service-worker.js
@@ -3,31 +3,23 @@ self.addEventListener('message', function(event) {
     const firebaseConfig = event.data.firebaseConfig;
 
     if (firebaseConfig) {
-        // Import Firebase scripts for messaging
         importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js');
         importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging.js');
 
-        // Inicializácia Firebase pomocou dynamicky prijatej konfigurácie
         firebase.initializeApp(firebaseConfig);
         console.log('Firebase initialized in Service Worker with config:', firebaseConfig);
 
-        // Inicializácia Firebase Messaging
         const messaging = firebase.messaging();
 
-        // Spracovanie prichádzajúcich push notifikácií na pozadí
         messaging.onBackgroundMessage(function(payload) {
             console.log('[ios-service-worker.js] Received background message ', payload);
-        
+
             const notificationTitle = payload.notification?.title || 'Default Title';
             const notificationOptions = {
                 body: payload.notification?.body || 'Default Body',
-                icon: '/static/img/icon.png'  // Definuj cestu k tvojej ikone
+                icon: '/static/img/icon.png'
             };
-        
-            // Pridáme log na overenie obsahu notifikácie
-            console.log('Notification content:', notificationTitle, notificationOptions);
-        
-            // Zobrazenie notifikácie
+
             self.registration.showNotification(notificationTitle, notificationOptions);
         });
     } else {
